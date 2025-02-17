4c4f1b97a0a578920f3785507ca2e510f092a116
haiyen11231 <haiyen11231@gmail.com>
2025-02-17 18:20:18 +0800

    :sparkles: feat: add friend_handler

--- internal/app/web_app/service/friend_handler.go
+++ internal/app/web_app/service/friend_handler.go
@@ -30,9 +30,35 @@ func (svc *WebService) FollowUser(ctx *gin.Context) {
 }
 
 func (svc *WebService) UnfollowUser(ctx *gin.Context) {
+	userId := ctx.GetUint64("user_id")
+	followingId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, &models.MessageResponse{Message: "Invalid user ID"})
+		return
+	}
 
+	response, err := svc.authenAndPostClient.UnfollowUser(ctx, &authen_and_post.UnfollowUserRequest{
+		UserId: userId,
+		FollowingId: uint64(followingId),
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, &models.MessageResponse{Message: err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
 
 func (svc *WebService) GetFollowerList(ctx *gin.Context) {
-	
+	userId := ctx.GetUint64("user_id")
+
+	response, err := svc.authenAndPostClient.GetFollowerList(ctx, &authen_and_post.GetFollowerListRequest{
+		UserId: userId,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, &models.MessageResponse{Message: err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
