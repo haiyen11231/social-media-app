8ae7603089f1b8b27cde19499c444b0ea7a9f542
haiyen11231 <haiyen11231@gmail.com>
2025-02-18 10:05:30 +0800

    :sparkles: feat: add newsfeed_handler

--- internal/app/web_app/service/newsfeed_handler.go
+++ internal/app/web_app/service/newsfeed_handler.go
@@ -1,7 +1,23 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/haiyen11231/social-media-app.git/internal/grpc/pb/newsfeed"
+	"github.com/haiyen11231/social-media-app.git/internal/models"
+)
 
 func (svc *WebService) GetNewsfeed(ctx *gin.Context) {
+	userId := ctx.GetUint64("user_id")
 
+	response, err := svc.newsfeedClient.GetNewsfeed(ctx, &newsfeed.GetNewsfeedRequest{
+		UserId: userId,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, &models.MessageResponse{Message: err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
